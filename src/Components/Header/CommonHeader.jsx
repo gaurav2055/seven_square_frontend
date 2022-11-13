390cfffdb5bf24710d5bb96aafddcd3efe4fe4ac
Gaurav2055 <jaygauravs@gmail.com>
2022-11-13 22:44:12 +0530

    Revert "Made the user Login and appropriate changes so that the user can navigate to onClickProperties pages only after logining in. and if the user is already login no need to login again. otp system works. only need to set up axios to send user data backend. Will do that in the next commit. Common landing page routes were not set so i set it for home and properties, Services still left as well as home|Rent|Sell."
    This reverts commit b02e1358c9fe331d39902a8aa25ebb0a7ab61cce.

--- src/Components/Header/CommonHeader.jsx
+++ src/Components/Header/CommonHeader.jsx
@@ -1,55 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-import seven_square_realtors_logo_white from "../../assets/seven_square_realtors_logo_white.png";
-import hamburger from "../../assets/hamburger.png";
+import seven_square_realtors_logo_white from '../../assets/seven_square_realtors_logo_white.png'
+import hamburger from '../../assets/hamburger.png'
 
 const CommonHeader = () => {
-	return (
-		<>
-			<nav
-				className='navbar navbar-expand-lg'
-				style={{
-					backgroundColor: "#000",
-				}}
-			>
-				<div className='container'>
-					<Link className='navbar-brand' to='/'>
-						<img className='' src={seven_square_realtors_logo_white} alt='logo' />
-					</Link>
-					<button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='true' aria-label='Toggle navigation'>
-						<span className='navbar-toggler-icon'>
-							<img src={hamburger} alt='' />
-						</span>
-					</button>
-					<div className='collapse navbar-collapse' id='navbarNav'>
-						<ul className='navbar-nav ms-auto'>
-							<li className='nav-item'>
-								<Link className='nav-link text-white' aria-current='page' to='/'>
-									Home
-								</Link>
-							</li>
-							<li className='nav-item'>
-								<Link className='nav-link text-white' to='/properties'>
-									Properties
-								</Link>
-							</li>
-							<li className='nav-item'>
-								<Link className='nav-link text-white' to='/.services'>
-									Services
-								</Link>
-							</li>
-							<li className='nav-item'>
-								<a className='nav-link text-white' href='/'>
-									Sell/Rent | List Property
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</nav>
-		</>
-	);
-};
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg" style={{
+        backgroundColor: '#000'
+      }}>
+        <div className="container">
+            <Link className="navbar-brand" to="/">
+                <img className='' src={seven_square_realtors_logo_white} alt='logo' />
+            </Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
+            <span className='navbar-toggler-icon'>
+              <img src={hamburger} alt="" />
+            </span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                <a className="nav-link text-white" aria-current="page" href="/">Home</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link text-white" href="/">Properties</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link text-white" href="/">Services</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link text-white" href='/'>Sell/Rent | List Property</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+      </nav>
+    </>
+  )
+}
 
-export default CommonHeader;
+export default CommonHeader
--- src/Components/PropertyListingPage/PropertiesListing.jsx
+++ src/Components/PropertyListingPage/PropertiesListing.jsx
@@ -1,133 +1,76 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../../Firebase/FBInit";
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-import UserLogin from "../UserLogin/UserLogin";
+import './PropertyListing.css'
 
-import "./PropertyListing.css";
-
-import imagesArray from "../../Data/Images";
+import imagesArray from '../../Data/Images'
 
 const PropertiesListing = () => {
-	const [show, setShow] = useState(false);
-	const navigate = useNavigate();
-	const OnClickPropertyHandler = () => {
-		onAuthStateChanged(auth, (user) => {
-			if (user) {
-				// User is signed in, see docs for a list of available properties
-				// https://firebase.google.com/docs/reference/js/firebase.User
-				// const uid = user.uid;
-				navigate("/property-details");
-				// ...
-			} else {
-				// User is signed out
-				// ...
-				setShow(true);
-			}
-		});
-	};
 
-	return (
-		<>
-			<div className='container mt-4'>
-				<div className='masonry'>
-					<div className='img-card' onClick={OnClickPropertyHandler}>
-						<img src={imagesArray[0].img} alt='propertyImg' />
-						<div className='overlay'>
-							<p className='h6 px-4 pt-3'> 1200 sqft. </p>
-							<p className='property px-4'>
-								{" "}
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis!{" "}
-							</p>
-						</div>
-					</div>
-					<div className='img-card mt-3'>
-						<img src={imagesArray[1].img} alt='propertyImg' />
-						<div className='overlay'>
-							<p className='h6 px-4 pt-3'> 1200 sqft. </p>
-							<p className='property px-4'>
-								{" "}
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis!{" "}
-							</p>
-						</div>
-					</div>
-					<div className='img-card mt-3'>
-						<img src={imagesArray[2].img} alt='propertyImg' />
-						<div className='overlay'>
-							<p className='h6 px-4 pt-3'> 1200 sqft. </p>
-							<p className='property px-4'>
-								{" "}
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis!{" "}
-							</p>
-						</div>
-					</div>
-					<div className='img-card mt-3'>
-						<img src={imagesArray[3].img} alt='propertyImg' />
-						<div className='overlay'>
-							<p className='h6 px-4 pt-3'> 1200 sqft. </p>
-							<p className='property px-4'>
-								{" "}
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis!{" "}
-							</p>
-						</div>
-					</div>
-					<div className='img-card mt-3'>
-						<img src={imagesArray[4].img} alt='propertyImg' />
-						<div className='overlay'>
-							<p className='h6 px-4 pt-3'> 1200 sqft. </p>
-							<p className='property px-4'>
-								{" "}
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis!{" "}
-							</p>
-						</div>
-					</div>
-					<div className='img-card mt-3'>
-						<img src={imagesArray[5].img} alt='propertyImg' />
-						<div className='overlay'>
-							<p className='h6 px-4 pt-3'> 1200 sqft. </p>
-							<p className='property px-4'>
-								{" "}
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis!{" "}
-							</p>
-						</div>
-					</div>
-					<div className='img-card mt-3'>
-						<img src={imagesArray[6].img} alt='propertyImg' />
-						<div className='overlay'>
-							<p className='h6 px-4 pt-3'> 1200 sqft. </p>
-							<p className='property px-4'>
-								{" "}
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis!{" "}
-							</p>
-						</div>
-					</div>
-				</div>
-				{show && (
-					<div className='modal modal-lg' tabIndex='-1' role='dialog'>
-						<div className='modal-dialog modal-dialog-centered' role='document'>
-							<div className='modal-content'>
-								<div className='modal-header'>
-									<button
-										type='button'
-										className='close'
-										onClick={() => {
-											setShow(false);
-										}}
-									>
-										<span aria-hidden='true'>&times;</span>
-									</button>
-								</div>
-								<div className='modal-body'>
-									<UserLogin />
-								</div>
-							</div>
-						</div>
-					</div>
-				)}
-			</div>
-		</>
-	);
-};
+  const navigate = useNavigate()
+  const OnClickPropertyHandler = ()=>{
+    navigate('/property-details')
+  }
 
-export default PropertiesListing;
+  return (
+    <>
+    <div className='container mt-4'>
+        <div className="masonry">
+
+            <div className="img-card" onClick={OnClickPropertyHandler}>
+              <img src={imagesArray[0].img} alt="propertyImg" />
+              <div className="overlay">
+                <p className='h6 px-4 pt-3'> 1200 sqft. </p>
+                <p className='property px-4'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis! </p>
+              </div>
+            </div>
+            <div className="img-card mt-3">
+              <img src={imagesArray[1].img} alt="propertyImg" />
+              <div className="overlay">
+                <p className='h6 px-4 pt-3'> 1200 sqft. </p>
+                <p className='property px-4'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis! </p>
+              </div>
+            </div>
+            <div className="img-card mt-3">
+              <img src={imagesArray[2].img} alt="propertyImg" />
+              <div className="overlay">
+                <p className='h6 px-4 pt-3'> 1200 sqft. </p>
+                <p className='property px-4'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis! </p>
+              </div>
+            </div>
+            <div className="img-card mt-3">
+              <img src={imagesArray[3].img} alt="propertyImg" />
+              <div className="overlay">
+                <p className='h6 px-4 pt-3'> 1200 sqft. </p>
+                <p className='property px-4'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis! </p>
+              </div>
+            </div>
+            <div className="img-card mt-3">
+              <img src={imagesArray[4].img} alt="propertyImg" />
+              <div className="overlay">
+                <p className='h6 px-4 pt-3'> 1200 sqft. </p>
+                <p className='property px-4'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis! </p>
+              </div>
+            </div>
+            <div className="img-card mt-3">
+              <img src={imagesArray[5].img} alt="propertyImg" />
+              <div className="overlay">
+                <p className='h6 px-4 pt-3'> 1200 sqft. </p>
+                <p className='property px-4'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis! </p>
+              </div>
+            </div>
+            <div className="img-card mt-3">
+              <img src={imagesArray[6].img} alt="propertyImg" />
+              <div className="overlay">
+                <p className='h6 px-4 pt-3'> 1200 sqft. </p>
+                <p className='property px-4'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis! </p>
+              </div>
+            </div>
+
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default PropertiesListing
