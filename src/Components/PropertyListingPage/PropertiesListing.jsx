4e736b383caa429b0dad6d14b28c4d49dee3bb26
Sayyam <sayyammshah@gmail.com>
2022-11-13 10:34:08 +0530

    UI Completed Left with functionality / Api Integration and admin part

--- src/Components/PropertyListingPage/PropertiesListing.jsx
+++ src/Components/PropertyListingPage/PropertiesListing.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom'
 
 import './PropertyListing.css'
 
+import imagesArray from '../../Data/Images'
+
 const PropertiesListing = () => {
 
   const navigate = useNavigate()
@@ -15,24 +17,55 @@ const PropertiesListing = () => {
     <div className='container mt-4'>
         <div className="masonry">
 
-            <div className="img-card">
-              <img src="https://images.unsplash.com/photo-1513694203232-719a280e022f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="propertyImg" />
-              <p className='h6 px-4 pt-3'> 1200 sqft. </p>
-              <p className='property px-4'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis! </p>
-              <div className="btn btn-dark mx-4 mb-3" onClick={OnClickPropertyHandler}>View Property</div>
+            <div className="img-card" onClick={OnClickPropertyHandler}>
+              <img src={imagesArray[0].img} alt="propertyImg" />
+              <div className="overlay">
+                <p className='h6 px-4 pt-3'> 1200 sqft. </p>
+                <p className='property px-4'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis! </p>
+              </div>
             </div>
-
-            <img src="https://images.unsplash.com/photo-1583847268964-b28dc8f51f92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG9tZSUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="propertyImg" />
-
-            <img src="https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1400&q=80" alt="propertyImg" />
-
-            <img src="https://images.unsplash.com/photo-1586023492125-27b2c045efd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aG9tZSUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="propertyImg" />
-
-            <img src="https://images.unsplash.com/photo-1521017432531-fbd92d768814?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FmZSUyMGl0ZXJpb3J8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="propertyImg" />
-
-            <img src="https://images.unsplash.com/photo-1618221639244-c1a8502c0eb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGhvbWUlMjBpbnRlcmlvcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="propertyImg" />
-
-            <img src="https://images.unsplash.com/photo-1493857671505-72967e2e2760?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2FmZSUyMGl0ZXJpb3J8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="propertyImg" />
+            <div className="img-card mt-3">
+              <img src={imagesArray[1].img} alt="propertyImg" />
+              <div className="overlay">
+                <p className='h6 px-4 pt-3'> 1200 sqft. </p>
+                <p className='property px-4'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis! </p>
+              </div>
+            </div>
+            <div className="img-card mt-3">
+              <img src={imagesArray[2].img} alt="propertyImg" />
+              <div className="overlay">
+                <p className='h6 px-4 pt-3'> 1200 sqft. </p>
+                <p className='property px-4'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis! </p>
+              </div>
+            </div>
+            <div className="img-card mt-3">
+              <img src={imagesArray[3].img} alt="propertyImg" />
+              <div className="overlay">
+                <p className='h6 px-4 pt-3'> 1200 sqft. </p>
+                <p className='property px-4'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis! </p>
+              </div>
+            </div>
+            <div className="img-card mt-3">
+              <img src={imagesArray[4].img} alt="propertyImg" />
+              <div className="overlay">
+                <p className='h6 px-4 pt-3'> 1200 sqft. </p>
+                <p className='property px-4'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis! </p>
+              </div>
+            </div>
+            <div className="img-card mt-3">
+              <img src={imagesArray[5].img} alt="propertyImg" />
+              <div className="overlay">
+                <p className='h6 px-4 pt-3'> 1200 sqft. </p>
+                <p className='property px-4'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis! </p>
+              </div>
+            </div>
+            <div className="img-card mt-3">
+              <img src={imagesArray[6].img} alt="propertyImg" />
+              <div className="overlay">
+                <p className='h6 px-4 pt-3'> 1200 sqft. </p>
+                <p className='property px-4'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius consectetur omnis! </p>
+              </div>
+            </div>
 
         </div>
     </div>
